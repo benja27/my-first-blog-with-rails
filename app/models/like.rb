9da95773411e1a24dd79d73b347d50796a0933fe
benja27 <50673336+benja27@users.noreply.github.com>
2023-08-25 17:12:56 -0600

    Add methods and crud

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
   belongs_to :author
   belongs_to :post
+
+
+ after_create :increment_likes_counter
+
+ def increment_likes_counter
+    post.increment!(:likes_counter)
+ end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,18 @@
 class Post < ApplicationRecord
-  belongs_to :author
+
+  belongs_to :author, class_name: 'User' 
+  has_many :comments
+  has_many :likes 
+  
+  after_create :increment_user_posts_count 
+  
+
+  def increment_user_posts_count
+    author.increment!(:posts_counter)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
 end
