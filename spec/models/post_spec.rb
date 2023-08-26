724d3732338ffc8fd650348b4a8f18f56db34105
benja27 <50673336+benja27@users.noreply.github.com>
2023-08-26 14:50:16 -0600

    Add more tests and fix linter errors

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,11 +4,33 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   it 'is valid with a name and email' do
-    # user = User.new(name: "Example User", email: "user@example.com")
     first_user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
 
     first_post = Post.new(author: first_user, title: 'Hello', text: 'This is my first post')
 
     expect(first_post).to be_valid
+  end
+
+  it 'is valid with an author, title, and text' do
+    user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    post = Post.new(author: user, title: 'Hello', text: 'This is my first post')
+
+    expect(post).to be_valid
+  end
+
+  it 'is not valid without an author' do
+    post = Post.new(title: 'Hello', text: 'This is my first post')
+    expect(post).not_to be_valid
+  end
+
+  it 'is not valid without a title' do
+    user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    post = Post.new(author: user, text: 'This is my first post')
+    expect(post).not_to be_valid
+  end
+
+  it 'belongs to an author' do
+    association = described_class.reflect_on_association(:author)
+    expect(association.macro).to eq :belongs_to
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject(:user) { described_class.new }
+
   it 'is valid with a name and email' do
-    # user = User.new(name: "Example User", email: "user@example.com")
     user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
     expect(user).to be_valid
+  end
+
+  it 'is valid with a present name' do
+    user.name = 'Example Name'
+    expect(user).to be_valid
+  end
+
+  it 'is not valid without a present name' do
+    user.name = nil
+    expect(user).not_to be_valid
+  end
+
+  it 'is not valid if posts_counter is not an integer' do
+    user.posts_counter = 1.5
+    expect(user).not_to be_valid
   end
 end
