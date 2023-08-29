74c4cf519c0ac23c797ff83973e4af6c5c2b7a04
benja27 <50673336+benja27@users.noreply.github.com>
2023-08-29 17:20:45 -0600

    Add testing

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,17 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/posts/index'
-      expect(response).to have_http_status(:success)
+
+  let(:user) { User.create(name: 'Heather', photo: 'https://images.unsplash.com/photo', bio: 'young beauty woman.') }
+  let(:post) { Post.create(author: user, title: 'sample title', text: 'sample post') }
+  
+  describe 'GET /users/:id/posts' do
+    it 'renders the corresponding list of posts of a given user' do
+      get user_posts_path(user)
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:index)      
+      expect(response.body).to include('posts') 
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/posts/show'
-      expect(response).to have_http_status(:success)
+  describe 'GET /users/:id/posts/:id' do
+    it 'renders the show template and includes placeholder text' do
+      get user_post_path(user, post)
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:show)      
+      expect(response.body).to include('here you can find the post itself') 
     end
   end
+  
 end
