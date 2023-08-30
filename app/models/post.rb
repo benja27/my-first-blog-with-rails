748da790263d98716dc4b77205f6cc1352c34399
benja27 <50673336+benja27@users.noreply.github.com>
2023-08-30 13:44:51 -0600

    Fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,8 +8,8 @@ class Post < ApplicationRecord
 
   after_save :increment_user_posts_count
 
-  def all_comments 
-    comments.all    
+  def all_comments
+    comments.all
   end
 
   def recent_comments
