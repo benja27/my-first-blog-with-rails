ef114990573367993d8db396f716c1e9945ab678
benja27 <50673336+benja27@users.noreply.github.com>
2023-08-30 23:46:28 -0600

    Added new form to create a new post

--- app/controllers/concerns/posts_controller.rb
+++ app/controllers/concerns/posts_controller.rb
@@ -9,4 +9,29 @@ class PostsController < ApplicationController
     @users = User.all
     @post = Post.find(params[:id])
   end
+
+  def new
+    @user = User.find(1)
+    @post = Post.new 
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+
+    @post = current_user.posts.build(post_params) # Assuming you have a relationship between users and posts
+  
+    if @post.save
+      # Handle successful creation, e.g., redirect to a show page
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+  
 end
