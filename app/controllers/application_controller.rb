a9430d7bb7f8623348e9931dc142d6608d9cdf21
benja27 <50673336+benja27@users.noreply.github.com>
2023-09-08 18:23:23 -0600

    Changes required by reviewer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-
-  def current_user
-    # @current_user = User.find(1)
-  end
-
-  # helper_method :current_user
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,8 @@
 #
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable  
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: true
